db58ad2b93a0a351f1931af56dea9320ba001220
Itoo <ryndak65@gmail.com>
2023-08-22 09:08:24 +0200

    cmt

---
+++ src/components/portfolio.jsx
@@ -0,0 +1,56 @@
+User
+import { Routes, Route, Link, Navigate } from 'react-router-dom'
+import React, { useContext } from 'react'
+import '/src/index.css'
+import { localUserDataContext } from '../context/localUserDataContext'
+import { fetchStockData } from '../fetchStockData'
+
+export default function Portfolio(props) {
+
+    const localUserData = useContext(localUserDataContext);
+    
+    const arrayofUserData = Object.entries(localUserData.own_shares);
+
+    const [stockData, setStockData] = React.useState(null);
+
+    React.useEffect(() => {
+        for (let i = 0; i < arrayofUserData.length; i++) {
+            let currentSymbol = arrayofUserData[i][0];
+            async function fetchData() {
+                const data = await fetchStockData(currentSymbol); // Replace with the desired stock symbol
+                if (data !== undefined) {
+                    setStockData(prev => ({
+                        ...prev,
+                        [currentSymbol]: data.c
+                    })); // Update state with fetched data
+                }
+            }
+            fetchData(); // Call the fetchData function when the component mounts
+        }
+    }, []);
+
+    console.log(stockData);
+
+    return (
+        <div>
+            <tr>
+                <th>Stock</th>
+                <th>Shares</th>
+                <th>Price</th>
+                <th>Total</th>
+            </tr>
+            <tbody>
+                {
+                    arrayofUserData.map((ele) => (
+                        <tr>
+                            <th>{ele[0]}</th>
+                            <th>{ele[1]}</th>
+                            <th>{stockData[ele[0]]}</th>
+                            <th>{stockData[ele[0]] * ele[1]}</th>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </div>
+    )
+}
